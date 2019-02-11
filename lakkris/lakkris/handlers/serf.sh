6f2f90f4e7aa8cf20dd189d0ed83df6487092982
LostLakkris <LostLakkris@users.noreply.github.com>
2019-02-10 23:27:25 -0800

    the extra cycle of serf after main service activity was bad... still need to think through this

--- lakkris/lakkris/handlers/serf.sh
+++ lakkris/lakkris/handlers/serf.sh
@@ -10,10 +10,6 @@ if [[ -x "${HANDLER_ROOT}/${SERF_EVENT}" ]]; then
 		exec 200>> "/tmp/lakkris-handler.lock"
 		flock -n -x 200
 		echo "${PAYLOAD}" | exec "${HANDLER_ROOT}/${SERF_EVENT}"
-		# Rerun the serf script again incase environment changed
-		if [[ -x /lakkris/start-pre/serf.sh ]]; then
-			bash /lakkris/start-pre/serf.sh
-		fi
 		flock -u 200
 	) &>/dev/null &
 fi
