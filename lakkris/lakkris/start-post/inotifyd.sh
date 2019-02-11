e35ca3af7890230e05af24568c3e543ac06def36
LostLakkris <LostLakkris@users.noreply.github.com>
2019-02-10 19:02:41 -0800

    Add some join events for nzbhydra

--- lakkris/lakkris/start-post/inotifyd.sh
+++ lakkris/lakkris/start-post/inotifyd.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 source /tmp/lakkris.env
 
-while [[ ! -d /var/run/s6/services/inotifyd ]]; do
+while [[ ! -d /var/run/s6/services/inotifyd || ! -s /tmp/lakkris.notify ]]; do
 	sleep 1s
 done
 
-s6-svc -wU -u /var/run/s6/services/inotifyd
+s6-svc -wu -u /var/run/s6/services/inotifyd
 s6-svwait -u /var/run/s6/services/inotifyd
