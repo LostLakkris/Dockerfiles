7b954783f9e6c7bb5f41a0defe798e8ccf63e439
LostLakkris <LostLakkris@users.noreply.github.com>
2019-02-10 20:15:56 -0800

    tweaking flock

--- lakkris/lakkris/handlers/inotifyd.sh
+++ lakkris/lakkris/handlers/inotifyd.sh
@@ -7,7 +7,7 @@ NOTIFY_FILE="/tmp/lakkris.notify"
 if [[ -e "${NOTIFY_FILE}" ]]; then
 	(
 		exec 204>> "/tmp/lakkris-inotify.lock"
-		flock -n -x 204
+		flock -x -w 2 204
 		RUN_SCRIPT=$(grep "^${object}:${event}," "${NOTIFY_FILE}" | cut -d',' -f2-)
 		if [[ -x "${RUN_SCRIPT}" ]]; then
 			bash "${RUN_SCRIPT}"
--- lakkris/lakkris/handlers/serf.sh
+++ lakkris/lakkris/handlers/serf.sh
@@ -11,7 +11,7 @@ fi
 if [[ -x "${HANDLER_ROOT}/${SERF_EVENT}" ]]; then
 	(
 		exec 203>> "/tmp/lakkris-serf.lock"
-		flock -n -x 203
+		flock -x -w 2 203
 		echo "${PAYLOAD}" | exec "${HANDLER_ROOT}/${SERF_EVENT}"
 		flock -u 203
 	) &>/dev/null &
