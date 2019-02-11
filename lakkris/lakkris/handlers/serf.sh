a7089cf0aacd8103876761dfdd7d369a19783046
LostLakkris <LostLakkris@users.noreply.github.com>
2019-02-10 23:17:58 -0800

    not all flocks support -w, shifting templates to proxy_pass direct to ip, variable has other odd behavior

--- lakkris/lakkris/handlers/serf.sh
+++ lakkris/lakkris/handlers/serf.sh
@@ -7,8 +7,8 @@ HANDLER_ROOT="/lakkris/events-serf/${LAKKRIS_SERVICE}"
 
 if [[ -x "${HANDLER_ROOT}/${SERF_EVENT}" ]]; then
 	(
-		exec 200>> "/tmp/lakkris-serf.lock"
-		flock -x -w 2 200
+		exec 200>> "/tmp/lakkris-handler.lock"
+		flock -n -x 200
 		echo "${PAYLOAD}" | exec "${HANDLER_ROOT}/${SERF_EVENT}"
 		# Rerun the serf script again incase environment changed
 		if [[ -x /lakkris/start-pre/serf.sh ]]; then
