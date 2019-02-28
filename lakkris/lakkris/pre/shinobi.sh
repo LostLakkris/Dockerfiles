73472ee4acd3aa739b5c7303970b65d9b4937ab4
LostLakkris <LostLakkris@users.noreply.github.com>
2019-02-27 21:49:32 -0800

    shinobi: set random cron-key if default

--- lakkris/lakkris/pre/shinobi.sh
+++ lakkris/lakkris/pre/shinobi.sh
@@ -9,7 +9,16 @@ if [[ -z "${LAKKRIS_WEBROOT}" ]]; then
 fi
 
 if [[ -e "${CONFIG_FILE}" ]]; then
-	CONFIG=$(jq --raw-output --arg home "/${LAKKRIS_WEBROOT}" --arg admin "/${LAKKRIS_WEBROOT}/admin" --arg super "/${LAKKRIS_WEBROOT}/super" '.webPaths.home=$home|.webPaths.admin=$admin|.webPaths.super=$super' "${CONFIG_FILE}")
+	CRON_KEY=$(jq -c --raw-output '.cron.key' "${CONFIG_FILE}")
+	if [[ "${CRON_KEY}" == "change_this_to_something_very_random__just_anything_other_than_this" ]]; then
+		CRON_KEY=$(cat /proc/sys/kernel/random/uuid)
+	fi
+	CONFIG=$(jq --raw-output \
+		--arg cronkey "${CRON_KEY}" \
+		--arg home "/${LAKKRIS_WEBROOT}" \
+		--arg admin "/${LAKKRIS_WEBROOT}/admin" \
+		--arg super "/${LAKKRIS_WEBROOT}/super" \
+		'.webPaths.home=$home|.webPaths.admin=$admin|.webPaths.super=$super|.cron.key=$cronkey' "${CONFIG_FILE}")
 	echo "${CONFIG}" > "${CONFIG_FILE}"
 
 	UPDATE="export LAKKRIS_WEBROOT=${LAKKRIS_WEBROOT}"
