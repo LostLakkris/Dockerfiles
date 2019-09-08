d8e07d45d4ff6e909d0e37b3ee1feceaf4483837
LostLakkris <LostLakkris@users.noreply.github.com>
2019-09-07 17:07:50 -0700

    ci: quick test on multiplatform builds

--- build.sh
+++ build.sh
@@ -7,10 +7,26 @@ docker version
 
 if [[ -n "${FROM}" ]]; then
 	echo "== Checking available origin ARCHs"
+	PLATFORMS=""
 	for PLAT in $(docker manifest inspect "${FROM}" | jq -c --raw-output '.manifests[].platform'); do
 		PLAT_OS=$(echo "${PLAT}" | jq -c --raw-output '.os')
 		PLAT_ARCH=$(echo "${PLAT}" | jq -c --raw-output '.architecture')
 		PLAT_VARIANT=$(echo "${PLAT}" | jq -c --raw-output '.variant')
 		echo "Detected remote: ${PLAT_OS} - ${PLAT_ARCH}"
+		if [[ -n "${PLATFORMS}" ]]; then
+			PLATFORMS+=","
+		fi
+		PLATFORMS+="${PLAT_OS}/${PLAT_ARCH}"
+		if [[ -n "${PLAT_VARIANT}" && "${PLAT_VARIANT}" != "null" ]]; then
+			PLATFORMS+="/${PLAT_VARIANT}"
+		fi
 	done
+	FROM=${FROM} templater Dockerfile.tmpl > Dockerfile
+	docker buildx create --name ${CONTAINER}
+	docker buildx use ${CONTAINER}
+	docker buildx ls
+#	docker buildx build --platform "${PLATFORMS}" -t "lostlakkris/${CONTAINER}:latest" --push .
+#	docker buildx imagetools inspect docker.io/lostlakkris/${CONTAINER}:latest
+	docker buildx build --platform "${PLATFORMS}" -t "lostlakkris/${CONTAINER}:latest" .
+	docker buildx imagetools inspect lostlakkris/${CONTAINER}:latest
 fi
