91d43a4f4e0abff7939193fafddc744080e2293a
LostLakkris <LostLakkris@users.noreply.github.com>
2019-10-29 20:43:15 -0700

    compose: starting some more logical docker-compose files, dropping the weave expectations

--- compose/test-weave.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
-cd ${DIR}
-
-docker-compose -H=unix:///var/run/weave/weave.sock \
-  -f docker-compose.weave.yml \
-  up -d --remove-orphans --build
