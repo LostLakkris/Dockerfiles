923730ba78221c51b638ee55da64100c424d9c8c
LostLakkris <LostLakkris@users.noreply.github.com>
2020-11-01 11:44:01 -0800

    webtrees: add php7-exif

--- webtrees/s6/setup/webtrees.sh
+++ webtrees/s6/setup/webtrees.sh
@@ -8,6 +8,7 @@ apk add --no-cache \
 	php7-fileinfo php7-gd php7-intl \
 	php7-zip php7-pdo php7-pdo_mysql \
 	sqlite php7-pdo_sqlite php7-pdo_pgsql \
-	php7-pdo_odbc php7-simplexml
+	php7-pdo_odbc php7-simplexml \
+	php7-exif
 
 git clone --single-branch --branch ${RELEASE} --depth 1 https://github.com/fisharebest/webtrees.git /app
